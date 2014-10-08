867740c3882b2d11795527d543a4019b46738154
Roman Larionov <romanylarionov@gmail.com>
2014-10-08 19:29:59 -0400

    GLEW now compiles with CMake. The window now pops up and everything is good in life.

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,16 @@
 // This is a GLFW built in macro. It tells it to include gl3.h instead of gl.h.
 #define GLFW_INCLUDE_GLCOREARB
 
+#ifdef __APPLE__
+# define __gl3_h_
+# define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
+#endif
+
 #include <iostream>
 #include "vector"
-#include "GLFW/glfw3.h"
 #include "glm/glm.hpp"
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
 
 // Function Prototypes.
 GLFWwindow* initWindow();
@@ -26,10 +32,10 @@ int main()
     if (!glfwInit())
         exit(EXIT_FAILURE);
 
-
-
-
     GLFWwindow* window = initWindow();
+
+    glewExperimental = GL_TRUE;
+    glewInit();
 
     // Main Event Loop.
     while (!glfwWindowShouldClose(window))
@@ -38,12 +44,9 @@ int main()
         if (glfwGetKey(window, GLFW_KEY_ESCAPE))
             glfwSetWindowShouldClose(window, GL_TRUE);
 
-
-
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
-
-    //GLuint program = glCreateProgram();
-
 
     glfwTerminate();
     return 0;
@@ -75,50 +78,71 @@ GLFWwindow* initWindow()
 /******************
     Classes.
  ******************/
+
 /*
+    SimpleMeshObject: This is an implementation of a very simplistic
+    object to be rendered. It stores all of the info on said object
+    and controls it's individual drawing every frame.
+*/
 
+class SimpleMeshObject
+{
+    public:
 
+        std::vector<GLuint>* buffers;
+
+        SimpleMeshObject()
+        {
+            buffers = new std::vector<GLuint>();
+        }
+
+        virtual ~SimpleMeshObject()
+        {
+        }
+
+        void draw()
+        {
+            //glDrawArrays(GL_TRIANGLES, 0, );
+        }
+};
+
+/*
     Scene: This stores information on the rendering process and stores a
     draw call to update the scene every frame.
+*/
 
 class Scene
 {
     public:
 
-        std::vector<SimpleMeshObject>* objects = new std::vector<SimpleMeshObject>();
+        std::vector<SimpleMeshObject>* objects;
+
+        Scene()
+        {
+            objects = new std::vector<SimpleMeshObject>();
+        }
+
+        virtual ~Scene()
+        {
+        }
 
         void addObjects(SimpleMeshObject object)
         {
             objects->push_back(object);
         }
 
+        /*
         void draw(auto uniformSetter, auto attributeSetter)
         {
             for (auto currObject = objects->begin(); currObject != objects->end(); currObject++)
                 currObject->draw(uniformSetter, attributeSetter);
         }
-
+        */
 };
 
 
-    SimpleMeshObject: This is an implementation of a very simplistic
-    object to be rendered. It stores all of the info on said object
-    and controls it's individual drawing every frame.
-
-class SimpleMeshObject
-{
-    public:
-
-        std::vector<GLuint>* buffers = new std::vector<GLuint>();
 
 
-        void draw(auto uniformSetter, auto attributeSetter)
-        {
-            glDrawArrays(GL_TRIANGLES, 0, );
-        }
-};
-
-*/
 
 
 
