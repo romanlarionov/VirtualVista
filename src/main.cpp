afcca85aa0b045270938bec592495a2578ada15d
Roman Larionov <romanylarionov@gmail.com>
2014-10-10 20:24:28 -0700

    Added a createProgram function.

--- src/main.cpp
+++ src/main.cpp
@@ -17,14 +17,14 @@
 # define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
 #endif
 
-#include <iostream>
-#include "vector"
+#include <vector>
 #include "glm/glm.hpp"
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
 // Function Prototypes.
 GLFWwindow* initWindow();
+GLuint createProgram(GLchar const* vertexSource, GLchar const* fragmentSource);
 
 int main()
 {
@@ -36,6 +36,19 @@ int main()
 
     glewExperimental = GL_TRUE;
     glewInit();
+
+    int numVerticies = 3;
+
+    float verticies[] = {
+         0.0f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f
+    };
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
 
     // Main Event Loop.
     while (!glfwWindowShouldClose(window))
@@ -68,4 +81,52 @@ GLFWwindow* initWindow()
 
     return window;
 }
+
+GLuint createProgram(GLchar const* vertexSource, GLchar const* fragmentSource)
+{
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+
+    auto shaderCompiled = [] (GLuint shader) -> bool
+    {
+        GLint *compileSuccess = NULL;
+        glGetShaderiv(shader, GL_COMPILE_STATUS, compileSuccess);
+
+        if (!compileSuccess)
+        {
+            printf("Error: failed to compile shader.\n");
+            glDeleteShader(shader);
+            return false;
+        }
+
+        return true;
+    };
+
+    if (!shaderCompiled(vertexShader) || !shaderCompiled(fragmentShader))
+        return 0;
+
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+
+    glLinkProgram(program);
+
+    GLint *linkSuccess = NULL;
+    glGetProgramiv(program, GL_LINK_STATUS, linkSuccess);
+
+    if (!linkSuccess)
+    {
+        printf("Error: program failed to link correctly.\n");
+        glDeleteProgram(program);
+        return 0;
+    }
+
+    return program;
+}
+
 
